c0d80ddc28d21701bc58dc1764b106995d2da026
Juan Sepulveda <me@juan.do>
2022-03-15 15:25:01 -0400

    Add css-loader to webapack

--- packages/cli-next/src/bundler/webpack_config.ts
+++ packages/cli-next/src/bundler/webpack_config.ts
@@ -9,39 +9,39 @@ import {BUNDLE_NAME} from '../settings';
  * some values may be difficult or not able to be serialized to text.
  */
 export interface WebpackSummaryOptions {
-    /** Set high level configuration for the bundler. */
-    mode: 'development' | 'production';
-    /** Root context of source to bundle. */
-    context: string;
-    /** Path to first module to execute in the produced bundle. */
-    entry: string;
-    /** Path on disk to write files produced while bundling. */
-    outputPath?: string;
-    /** When set to true, we always generate a separate .map file with sourcemap information.
-     *  This overrides the default behavior based on mode where development would generate inline sourcemaps.
-     */
-    shouldGenerateSeparateSourceMaps?: boolean;
-    /** Configure live reload client. */
-    liveReload?: {
-        /**
-         * Connect the live reload client over an encrypted connection?
-         *
-         * @default true
-         */
-        https?: boolean;
-        port: number;
-    };
-    /** Definitions of file types to include in the bundle and how. */
-    assets: {
-        javascript: JavascriptAssetOptions;
-    };
+	/** Set high level configuration for the bundler. */
+	mode: 'development' | 'production';
+	/** Root context of source to bundle. */
+	context: string;
+	/** Path to first module to execute in the produced bundle. */
+	entry: string;
+	/** Path on disk to write files produced while bundling. */
+	outputPath?: string;
+	/** When set to true, we always generate a separate .map file with sourcemap information.
+	 *  This overrides the default behavior based on mode where development would generate inline sourcemaps.
+	 */
+	shouldGenerateSeparateSourceMaps?: boolean;
+	/** Configure live reload client. */
+	liveReload?: {
+		/**
+		 * Connect the live reload client over an encrypted connection?
+		 *
+		 * @default true
+		 */
+		https?: boolean;
+		port: number;
+	};
+	/** Definitions of file types to include in the bundle and how. */
+	assets: {
+		javascript: JavascriptAssetOptions;
+	};
 }
 
 export interface JavascriptAssetOptions {
-    assetType: 'javascript';
+	assetType: 'javascript';
 
-    transpiler: 'babel';
-    options: any;
+	transpiler: 'babel';
+	options: any;
 }
 
 /**
@@ -51,18 +51,18 @@ export interface JavascriptAssetOptions {
  * @param liveReload Configure live reload client.
  */
 function injectLiveReloadClient(entry: string, liveReload: WebpackSummaryOptions['liveReload']) {
-    let otherScripts = {};
-    if (liveReload) {
-        const clientPath = require.resolve('./live-reload-and-report-disconnection');
-        const protocol = liveReload.https ?? true ? 'https' : 'http';
-        otherScripts = {
-            'live-reload-and-report-disconnection': `${clientPath}?${protocol}://localhost:${liveReload.port}'`,
-        };
-    }
-    return {
-        [BUNDLE_NAME]: entry,
-        ...otherScripts,
-    };
+	let otherScripts = {};
+	if (liveReload) {
+		const clientPath = require.resolve('./live-reload-and-report-disconnection');
+		const protocol = liveReload.https ?? true ? 'https' : 'http';
+		otherScripts = {
+			'live-reload-and-report-disconnection': `${clientPath}?${protocol}://localhost:${liveReload.port}'`,
+		};
+	}
+	return {
+		[BUNDLE_NAME]: entry,
+		...otherScripts,
+	};
 }
 
 /**
@@ -72,52 +72,56 @@ function injectLiveReloadClient(entry: string, liveReload: WebpackSummaryOptions
  * @param param0 summary of webpack compiler config
  */
 export function createWebpackCompilerConfig({
-    mode,
-    context,
-    entry,
-    outputPath,
-    shouldGenerateSeparateSourceMaps,
-    liveReload,
-    assets: {
-        javascript: {options: babelOptions},
-    },
+	mode,
+	context,
+	entry,
+	outputPath,
+	shouldGenerateSeparateSourceMaps,
+	liveReload,
+	assets: {
+		javascript: {options: babelOptions},
+	},
 }: WebpackSummaryOptions): Configuration {
-    let devtool;
+	let devtool;
 
-    if (mode === 'development') {
-        devtool = 'inline-cheap-module-source-map';
-    }
-    if (shouldGenerateSeparateSourceMaps) {
-        devtool = 'source-map';
-    }
+	if (mode === 'development') {
+		devtool = 'inline-cheap-module-source-map';
+	}
+	if (shouldGenerateSeparateSourceMaps) {
+		devtool = 'source-map';
+	}
 
-    return {
-        mode,
-        devtool,
-        context,
-        entry: injectLiveReloadClient(entry, liveReload),
-        output: {
-            path: outputPath,
-        },
-        resolve: {
-            extensions: ['.ts', '.tsx', '.mjs', '.mjsx', '.js', '.jsx'],
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(?:m?j|t)sx?$/,
-                    include: [/node_modules/],
-                    resolve: {
-                        fullySpecified: false,
-                    },
-                },
-                {
-                    test: /\.(?:m?j|t)sx?$/,
-                    exclude: [/node_modules/],
-                    loader: require.resolve('babel-loader'),
-                    options: babelOptions,
-                },
-            ],
-        },
-    };
+	return {
+		mode,
+		devtool,
+		context,
+		entry: injectLiveReloadClient(entry, liveReload),
+		output: {
+			path: outputPath,
+		},
+		resolve: {
+			extensions: ['.ts', '.tsx', '.mjs', '.mjsx', '.js', '.jsx'],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(?:m?j|t)sx?$/,
+					include: [/node_modules/],
+					resolve: {
+						fullySpecified: false,
+					},
+				},
+				{
+					test: /\.(?:m?j|t)sx?$/,
+					exclude: [/node_modules/],
+					loader: require.resolve('babel-loader'),
+					options: babelOptions,
+				},
+				{
+					test: /\.css$/i,
+					use: ['css-loader'],
+				},
+			],
+		},
+	};
 }
